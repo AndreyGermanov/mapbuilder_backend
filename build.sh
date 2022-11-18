2f65665b89ffe0f339496c609042f25b2b398d84
Andrey Germanov <andrey@germanov.dev>
2022-11-18 17:30:52 +0300

    Deploy

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 systemctl stop mapbuilder
 mkdir -p /opt/mapbuilder
 /usr/local/go/bin/go build -o mapbuilder cmd/mapbuilder/mapbuilder.go
-mv -f ir /opt/mapbuilder/
-ln -s geodata /opt/mapbuilder/geodata
+mv -f mapbuilder /opt/mapbuilder/
+ln -sf geodata /opt/mapbuilder/geodata
 systemctl start mapbuilder
