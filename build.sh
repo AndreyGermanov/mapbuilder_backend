d4109ab674593d8d4ee6eaec5d7447734d5c1987
Andrey Germanov <andrey@germanov.dev>
2022-11-21 23:29:38 +0300

    Cleanup

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-systemctl stop mapbuilder
-mkdir -p /opt/mapbuilder
-/usr/local/go/bin/go build -o mapbuilder cmd/mapbuilder/mapbuilder.go
-mv -f mapbuilder /opt/mapbuilder/
-ln -sf geodata /opt/mapbuilder/geodata
-systemctl start mapbuilder
--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-git push origin master
-ssh root@test.germanov.dev "cd /root/mapbuilder_backend/;git pull origin master;./build.sh"
